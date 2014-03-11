3f1e4b2f7a1212b2a336c6289c5001ee902b0162
Ehtan Lee <mhb8436@airplug.com>
2014-03-11 09:40:26 +0900

    modified:   rest_review/google.py 	rest_review/ppompu.py

--- rest_review/google.py
+++ rest_review/google.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import csv
 import sys
 import re
@@ -7,6 +9,11 @@ import urllib2
 import json
 import gzip
 from StringIO import StringIO
+import sqlite3
+
+db = sqlite3.connect(':memory:')
+db.text_factory = str
+
 
 def main():
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
@@ -20,7 +27,7 @@ def main():
 
 	google_res_url = """https://www.google.co.kr/search?newwindow=1&sclient=psy-ab&q=%EC%97%AD%EC%82%BC%EB%8F%99+%EB%A7%9B%EC%A7%91&oq=%EC%97%AD%EC%82%BC%EB%8F%99+%EB%A7%9B%EC%A7%91&gs_l=hp.3..35i39l2j0i5i30l2.203768.206002.18.206309.16.16.0.0.0.8.215.2853.0j12j4.16.0....0...1c.1j4.37.psy-ab..41.124.18177.18dIgU7m6MM&pbx=1&bav=on.2,or.r_cp.r_qf.&bvm=bv.62578216%2Cd.aGc%2Cpv.xjs.s.en_US.Qb9R7Hul644.O&fp=fa3f9b48e84c6c&biw=1132&bih=606&tch=1&ech=1&psi=la4dU9C_A4mSiAeLvoGQBw.1394453883695.31"""
 	google_res = opener.open(google_res_url)
-
+	# urllists = []
 	print google_res.info().get('Content-Encoding')
 	if google_res.info().get('Content-Encoding') == 'gzip':
 		buf = StringIO( google_res.read() )
@@ -33,6 +40,123 @@ def main():
 		urls = [ url.replace('amp;u=','') for url in urls]
 		urls = [ url.replace('&amp;','?') for url in urls]
 		print '\n'.join(urls)
+	return urls
+
+def idExists(url):
+	cursor = db.cursor()
+	cursor.execute(""" select count(*) from toprestrunt where url = '"""+url +"""'""")
+	row = cursor.fetchone()
+	if row:
+		return True
+	else:
+		return False
+
+def update(info):
+	curosr = db.curosr()
+	curosr.execute(""" update toprestrunt set score = ? where url = ? """, (info.score, info.url))
+	db.commit()
+
+def insert(info):
+	cursor = db.cursor()
+	print info
+	if idExists(info['url']):
+		# update(line)
+		print 'update(line)'
+	else:
+		now = datetime.datetime.now()
+		try:
+			cursor.execute('''
+				INSERT INTO toprestrunt(url, title, score, ts)
+		    VALUES(?,?,?,?)''', (info.url, info.title, info.score, now)
+		    )
+		except ValueError as e:
+			pass
+
+def parser(urls):
+	for url in urls:
+		content = get_info_from_url(url)
+		insert(filter(url, content))
+
+def filter(url, content):
+	# print content
+	info = {}
+	info['url'] = url
+	info['score'] = int(5)
+	try:
+		titfil = re.search(r'<title[^>]*>(.*?)</title>',content)
+		info['title'] = titfil.group(0)
+	except:
+		info['title'] = ''
+	
+	try:
+		telfil = re.search(r'.*[전].*[화].*[번].*[호].*', content)
+		if telfil:
+			info['score'] = int(-1)
+	except:
+		pass
+	# print telfil
+	try:
+		adrfil = re.search(r'.*[주].*[소].*', content)
+		if adrfil:
+			info['score'] = int(-1)
+	except:
+		pass
+
+
+	# print info
+	return info
+
+def get_info_from_url(url):
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+	urllib2.install_opener(opener)
+	opener.addheaders = [
+		 ('accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+		,('accept-encoding','gzip,deflate,sdch')
+		,('accept-language','en-US,en;q=0.8,ko;q=0.6')
+		,('user-agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36')
+	]
+	try:
+		res = opener.open(url)
+		print res.info().get('Content-Encoding')
+		if res.info().get('Content-Encoding') == 'gzip':
+			buf = StringIO( res.read() )
+			f = gzip.GzipFile(fileobj=buf)
+			data = f.read()
+			return data
+	except:
+		pass
+
+def drop_table():
+	cursor = db.cursor()
+	cursor.execute('''
+		DROP TABLE IF EXISTS toprestrunt
+		''')
+	db.commit()
+
+def create_table():
+	cursor = db.cursor()
+	cursor.execute('''
+    CREATE TABLE IF NOT EXISTS toprestrunt(url VARCHAR(500), title VARCHAR(500),
+                       score INTEGER, ts timestamp)
+	''')
+	db.commit()
+
+
+def get():
+	cursor = db.cursor()
+	cursor.execute(""" select * from toprestrunt """)
+	all_rows = cursor.fetchall()
+	for row in all_rows:
+		print('{0} : {1} : {2} : {3}'.format(row[0], row[1], row[2], row[3]))
+
 
 if __name__ == '__main__':
-	main()
+	drop_table()
+	create_table()
+	print '-------- create table ------'
+	urls = main()
+	print '--------- parser table -------'
+	parser(urls)
+	print '--------- get table -------'
+	get()
+
