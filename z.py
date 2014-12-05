6a292bc338f550cf8b183ccc7e51eac7ef317f8b
mhb8436@gmail.com <mhb8436@gmail.com>
2014-12-05 09:19:56 +0900

    new file:   z.py 	list.html 	test.html

---
+++ z.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import sys
+import re
+import urllib
+import urllib2
+# import simplejson as json
+import json
+import gzip
+from StringIO import StringIO
+import sqlite3
+import datetime
+
+
+def get_data_from_url(url):
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+	urllib2.install_opener(opener)
+	opener.addheaders = [
+		 ('Accept','text/plain, */*; q=0.01')
+		,('Accept-encoding','gzip, deflate, sdch')
+		,('Accept-language','ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4')
+		,('Referer',url)
+		,('Cookie', 'PHPSESSID=a4a130a2d7812596e0fa29a7b248578f;')
+		,('X-Requested-With', 'XMLHttpRequest')
+		,('Connection','keep-alive')
+		,('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36')
+	]
+	try:
+		res = opener.open(url)
+		if res.info().get('Content-Encoding') == 'gzip':
+			buf = StringIO( res.read() )
+			f = gzip.GzipFile(fileobj=buf)
+			data = f.read()
+			return data
+	except:
+		pass
+
+def parse_category(content):
+	data = re.findall(r'<section class="list">[\s\r\n]*<ul>[\s\w\"\n\r\=\<\>\/\W]*</ul>[\s\r\n]*</section>', content, re.M|re.I)
+	for i, d in enumerate(data):
+		try:
+			title = re.findall(r'(?<=<div class="txt">)[\W+]*(?=</div>)', d, re.M|re.I)
+			for j, t in enumerate(title):
+				print t
+			url = re.findall(r'(?<=href=")[\w\s\d\/]*', d, re.M|re.I)
+			for j, u in enumerate(url):
+				print u
+		except IndexError:
+			pass 
+
+
+def parse_pod_from_category(content):
+	print content
+
+if __name__ == '__main__':
+	# parse_category(get_data_from_url("http://m.podbbang.com/category")) # parse category
+	parse_pod_from_category(get_data_from_url("http://m.podbbang.com/category/lists/0/1"))
+
