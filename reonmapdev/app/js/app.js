f793fbac7d8e823ab34a3ac5909eaab1c9705cda
Ethan <mhb8436@airplug.com>
2015-11-04 17:36:44 +0900

    modified:   reonmapdev/app/js/app.js 	new file:   reonmapdev/data/jj01.json 	new file:   reonmapdev/data/jj03_20110101_20120101.json 	new file:   reonmapdev/data/jj03_20110201_20120201.json 	new file:   reonmapdev/data/jj03_20110301_20120301.json 	new file:   reonmapdev/data/jj03_20110401_20120401.json 	new file:   reonmapdev/data/jj03_20110501_20120501.json 	new file:   reonmapdev/data/jj03_20110601_20120601.json 	new file:   reonmapdev/data/jj03_20110701_20120701.json 	new file:   reonmapdev/data/jj03_20110801_20120801.json 	new file:   reonmapdev/data/jj03_20110901_20120901.json 	new file:   reonmapdev/data/jj03_20111001_20121001.json 	new file:   reonmapdev/data/jj03_20111101_20121101.json 	new file:   reonmapdev/data/jj03_20111201_20121201.json 	new file:   reonmapdev/data/jj03_20120101_20130101.json 	new file:   reonmapdev/data/jj03_20120201_20130201.json 	new file:   reonmapdev/data/jj03_20120301_20130301.json 	new file:   reonmapdev/data/jj03_20120401_20130401.json 	new file:   reonmapdev/data/jj03_20120501_20130501.json 	new file:   reonmapdev/data/jj03_20120601_20130601.json 	new file:   reonmapdev/data/jj03_20120701_20130701.json 	new file:   reonmapdev/data/jj03_20120801_20130801.json 	new file:   reonmapdev/data/jj03_20120901_20130901.json 	new file:   reonmapdev/data/jj03_20121001_20131001.json 	new file:   reonmapdev/data/jj03_20121101_20131101.json 	new file:   reonmapdev/data/jj03_20121201_20131201.json 	new file:   reonmapdev/data/jj03_20130101_20140101.json 	new file:   reonmapdev/data/jj03_20130201_20140201.json 	new file:   reonmapdev/data/jj03_20130301_20140301.json 	new file:   reonmapdev/data/jj03_20130401_20140401.json 	new file:   reonmapdev/data/jj03_20130501_20140501.json 	new file:   reonmapdev/data/jj03_20130601_20140601.json 	new file:   reonmapdev/data/jj03_20130701_20140701.json 	new file:   reonmapdev/data/jj03_20130801_20140801.json 	new file:   reonmapdev/data/jj03_20130901_20140901.json 	new file:   reonmapdev/data/jj03_20131001_20141001.json 	new file:   reonmapdev/data/jj03_20131101_20141101.json 	new file:   reonmapdev/data/jj03_20131201_20141201.json 	new file:   reonmapdev/data/jj03_20140101_20150101.json 	new file:   reonmapdev/data/jj03_20140201_20150201.json 	new file:   reonmapdev/data/jj03_20140301_20150301.json 	new file:   reonmapdev/data/jj03_20140401_20150401.json 	new file:   reonmapdev/data/jj03_20140501_20150501.json 	new file:   reonmapdev/data/jj03_20140601_20150601.json 	new file:   reonmapdev/data/jj03_20140701_20150701.json 	new file:   reonmapdev/data/jj03_20140801_20150801.json 	new file:   reonmapdev/data/jj03_20140901_20150901.json 	new file:   reonmapdev/data/jj03_20141001_20151001.json 	new file:   reonmapdev/data/jj03_20141101_20151101.json 	new file:   reonmapdev/data/jj03_20141201_20151201.json 	modified:   reonmapdev/dev.js

--- reonmapdev/app/js/app.js
+++ reonmapdev/app/js/app.js
@@ -1,15 +1,23 @@
 angular.module('deitel', ['ngRoute','ngSanitize','deitel.directives', 'deitel.services','deitel.controllers'])
 .config(['$routeProvider', '$locationProvider','$httpProvider', function($routeProvider, $locationProvider,$httpProvider) {
 
-	$routeProvider.
-		when('/sample', {
+	$routeProvider
+		.when('/sample', {
+			controller: 'SampleCtrl' ,
+			templateUrl: '/views/sample.html'
+		})
+		.when('/s01', {
+			controller: 'SampleCtrl' ,
+			templateUrl: '/views/sample.html'
+		})
+		.when('/s02', {
 			controller: 'SampleCtrl' ,
 			templateUrl: '/views/sample.html'
 		})
 
-		.otherwise({
-			redirectTo: '/sample'
-		});
+		// .otherwise({
+		// 	redirectTo: '/sample'
+		// });
 	$locationProvider.html5Mode(true);
 	// $httpProvider.interceptors.push('TokenInterceptor');
 }]);
--- reonmapdev/dev.js
+++ reonmapdev/dev.js
@@ -12,11 +12,19 @@ var numCPUs = require('os').cpus().length;
 
 var app = express()
 // console.log(__dirname);
-app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/'));
+app.use('data', express.static(__dirname + '/data'));
 
 app.get('/', function (req, res) {
-  res.sendFile('inddex.html',{ root: path.join(__dirname, '/views') })
+  res.sendFile('index.html',{ root: path.join(__dirname, '/') })
+});
+
+app.get(/s/, function (req, res) {
+  res.sendFile('index.html',{ root: path.join(__dirname, '/') })
+});
+
+app.get(/api/, function (req, res) {
+  res.sendFile('json',{ root: path.join(__dirname, '/data') })
 });
 
 var server = app.listen(9988, function () {
