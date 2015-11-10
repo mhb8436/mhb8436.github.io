f52c582b1d332be7334408670f2451a1cefa034e
Ethan <mhb8436@airplug.com>
2015-11-10 15:13:01 +0900

    modified:   Gruntfile.js 	modified:   app/js/controllers.js 	modified:   bin/lib_deitel.css 	modified:   bin/lib_deitel.js 	modified:   bin/lib_deitel.min.js 	modified:   data/jj01.json 	modified:   index.html 	new file:   vendor/js/angular-nvd3.js 	modified:   views/sample.html

--- reonmapdev/app/js/controllers.js
+++ reonmapdev/app/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('deitel.controllers', [])
+angular.module('deitel.controllers', ['nvd3'])
 
 .controller('SampleCtrl', 
 	function($window,$timeout,$location,$scope,$routeParams,$filter,$log,$sampleservice,$burl,$base){
@@ -23,7 +23,7 @@ angular.module('deitel.controllers', [])
 						continuousWorld: true,
 						worldCopyJump: false,
 						zoomControl: true
-					}).setView([37.49800253054263,127.02608766689583], 10),
+					}).setView([37.49800253054263,127.02608766689583], 9),
 		$scope.focusmap.doubleClickZoom.disable();
 		$scope.focusmap.scrollWheelZoom.disable();
 
@@ -45,7 +45,7 @@ angular.module('deitel.controllers', [])
 			// var ext = d3.extent(data,function(d){ return d.value; });
 			// console.log(ext);
 			var options = {
-				radius: 10,
+				radius: 15,
 				opacity: .5,
 				duration: 200,
 				lng: function(d){ return d.lng; },
@@ -62,8 +62,9 @@ angular.module('deitel.controllers', [])
 					var cclng = d3.mean(d, function(c){ return c.o['lng']; }),
 							cclat = d3.mean(d, function(c){ return c.o['lat']; });
 							console.log(cclng + ':' + cclat);
-					$scope.focusmap.setView([cclat,cclng], 10); 
+					$scope.focusmap.setView([cclat,cclng], 9); 
 					console.log(d); $scope.drawDetailMap(d); 
+
 				}
 			}
 			var hexLayer = L.hexbinLayer(options).addTo($scope.map);
@@ -91,7 +92,7 @@ angular.module('deitel.controllers', [])
 			});
 
 			data.forEach(function(d){
-				var info = '<table class="table table-striped table-hover" style="color:#000"><thead><tr class="info" style="color:#000"><td>아파트명</td><td>거래량</td></tr></thead><tbody><tr><td>'+d.o['aptnm']+')'+'</td><td>'+Math.round(d.o['value'])+'</td></tr></tbody></table>';
+				var info = '<table class="table table-striped table-hover" style="color:#000"><thead><tr class="info" style="color:#000"><td>아파트명</td><td>거래량</td></tr></thead><tbody><tr><td><a href="#" onclick="drawChart(\''+d.o['series']+'/'+d.o['si_series']+'/'+d.o['gu_series']+'\')">'+d.o['aptnm']+')'+'</a></td><td>'+Math.round(d.o['value'])+'</td></tr></tbody></table>';
 				markers.addLayer(
 					new L.Marker(new L.LatLng(d.o['lat'],d.o['lng']),{ icon: new SweetIcon({ labelText: d.o['aptnm'] }) }).bindPopup(info)
 					);
@@ -99,4 +100,96 @@ angular.module('deitel.controllers', [])
 			$scope.detailmarkerlst.push(markers);
 			$scope.focusmap.addLayer(markers);
 		}
+		// $scope.ddd = 1;
+
+		$scope.options = {
+      chart: {
+        type: 'multiBarChart',
+        height: 650,
+        margin : {
+          top: 20,
+          right: 20,
+          bottom: 60,
+          left: 65
+        },
+        x: function(d){ return (new Date(d[0].substr(0,4), d[0].substr(4,2), d[0].substr(6,2))).getTime() ; },
+        y: function(d){ return parseFloat(d[1]); },
+        // average: function(d) { return d.mean; },
+        color: d3.scale.category10().range(),
+        transitionDuration: 300,
+        stacked: false,
+        duration: 500,
+        xAxis: {
+          axisLabel: '년월일',
+          tickFormat: function(d) {
+            return d3.time.format('%Y%m%d')(new Date(d));
+          },
+          showMaxMin: true,
+          staggerLabels: true
+        },
+
+        yAxis: {
+          axisLabel: '거래량(건)',
+          tickFormat: function(d){
+              return d3.format('d')(d);
+          },
+          showMaxMin: true,
+          axisLabelDistance: -20
+        }
+      }
+    };
+
+		$window.drawChart = function(data){
+			// console.log(data);
+			var datecomp = function(b,a){
+				return new Date(b.split(':')[0].substr(0,4)+'/'+b.split(':')[0].substr(4,2)+'/01') - new Date(a.split(':')[0].substr(0,4)+'/'+a.split(':')[0].substr(4,2)+'/01');
+			};
+
+			var my = data.split('/')[0].split(','),
+				  si = data.split('/')[1].split(','),
+				  gu = data.split('/')[2].split(',');
+			si.sort(datecomp), gu.sort(datecomp);		
+			// console.log(si);
+			var fullm = {}, minm = moment(new Date(parseInt(si[0].split(':')[0].substr(0,4)), parseInt(si[0].split(':')[0].substr(4,2))-1, 1));
+			console.log(si.length);
+			for(var k=0;k<si.length;k++){
+				fullm[minm.format('YYYYMMDD')] = {si:0,gu:0,my:0};
+				minm.add(1,'month');
+			}
+
+			for(var k=0;k<si.length;k++){
+				// console.log(si[k].split(':')[0]+'01');
+				fullm[si[k].split(':')[0]+'01']['si'] =  si[k].split(':')[1];
+			}
+			for(var k=0;k<gu.length;k++){
+				fullm[gu[k].split(':')[0]+'01']['gu'] =  gu[k].split(':')[1];
+			}
+			for(var k=0;k<my.length;k++){
+				fullm[my[k].split(':')[0]+'01']['my'] =  my[k].split(':')[1];
+			}
+			var data = [];
+			var s1 =[], s2=[],s3=[];
+			Object.keys(fullm).map(function(d){
+				s1.push([d, fullm[d]['my']]);
+				s2.push([d, fullm[d]['si']]);
+				s3.push([d, fullm[d]['gu']]);
+			})
+
+			$scope.$apply(function(){
+				// $scope.data = [];
+				$scope.data = [{key:'아파트',values:s1},{key:'시평균',values:s2},{key:'구평균',values:s3}];
+				console.log($scope.data);		
+
+			});
+
+			$timeout(function(){
+					$scope.api.update();
+					$scope.api.refresh();
+					console.log('updated... but not..');
+				},500);		
+		}
+
+
+
+  
 });
